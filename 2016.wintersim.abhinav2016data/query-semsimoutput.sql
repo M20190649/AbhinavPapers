7644b967352f2885a8b9951eeb77e6bc8ec280c5
Abhinav Sunderrajan <abhinav.sunderrajan@ip-10-2-41-70.ap-southeast-1.compute.internal>
2019-04-18 10:20:15 +0800

    first commit

---
+++ 2016.wintersim.abhinav2016data/query-semsimoutput.sql
@@ -0,0 +1,8 @@
+﻿select sum(SUBQUERY.nt) from
+(select count(distinct(agent_id)) as nt,time_stamp from semsim_output where iteration_count =7
+and time_stamp >900 and time_stamp <2000 group by time_stamp) AS SUBQUERY
+
+
+
+
+select count(*) from semsim_output where iteration_count =1
